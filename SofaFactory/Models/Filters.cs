4ed16cd9ada4e8a73a1e6ad93476b3618b7d9441
gak72 <77842100+gak72@users.noreply.github.com>
2023-08-10 17:18:12 +0530

    filter for query string added or ajax

---
+++ SofaFactory/Models/Filters.cs
@@ -0,0 +1,12 @@
+﻿namespace SofaFactory.Models
+{
+    public class Filters
+    {
+        public List<string>? Materials { get; set; }
+        public List<string>? SeatingCapacities { get; set; }
+        public List<string>? Sizes { get; set; }
+        public List<string>? StorageTypes { get; set; }
+        public decimal MinPrice { get; set; } = 0;
+        public decimal MaxPrice { get; set; } = 0;
+    }
+}
