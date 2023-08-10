6880a5681ba1487ed4f5ac97eb4fb25c0a989e44
gak72 <77842100+gak72@users.noreply.github.com>
2023-08-10 17:09:31 +0530

    query string helper added

---
+++ SofaFactory/Helper/QueryStringHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using System;
+using System.Collections.Generic;
+
+namespace SofaFactory.Helper
+{
+    public static class QueryStringHelper
+    {
+        public static string UpdateQueryStringParameters(string queryString, IDictionary<string, string> parametersToUpdate)
+        {
+            // Convert query string to a dictionary
+          
+            IDictionary<string, string> queryParams = QueryHelpers.ParseQuery(queryString)
+                .ToDictionary(kv => kv.Key, kv => kv.Value.ToString());
+
+            // Update or add the specified parameters
+            foreach (var kvp in parametersToUpdate)
+            {
+                if (queryParams.ContainsKey(kvp.Key))
+                {
+                    queryParams[kvp.Key] = kvp.Value;
+                }
+                else
+                {
+                    queryParams.Add(kvp.Key, kvp.Value);
+                }
+            }
+
+            // Convert dictionary back to query string
+            string newQueryString = QueryHelpers.AddQueryString("", queryParams);
+
+            return newQueryString;
+        }
+    }
+}
