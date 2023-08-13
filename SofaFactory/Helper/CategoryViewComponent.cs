fc661c2d73552e3e152d7d721eed7f697b86dc3e
gak72 <77842100+gak72@users.noreply.github.com>
2023-08-13 13:43:29 +0530

    View component for category drop down

---
+++ SofaFactory/Helper/CategoryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SofaFactory.Data;
+
+namespace SofaFactory.Helper
+{
+    public class CategoryViewComponent:ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _context.Categories.ToList();
+            return View(items);
+        }
+
+    }
+}
