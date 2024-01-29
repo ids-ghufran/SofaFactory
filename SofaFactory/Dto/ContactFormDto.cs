7cd528c741b3b0a047f94f7ba399ce8c2e391db7
shahbazshaikh <shahbazstshaikh@gmail.com>
2024-01-29 15:46:27 +0530

    Contact form & enquery form, email send api and ajax request in html file done

---
+++ SofaFactory/Dto/ContactFormDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SofaFactory;
+
+public class ContactFormDto
+{
+    public string First { get; set; }
+    public string? Last { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Message { get; set; }
+}
---
+++ SofaFactory/Dto/FnDEnqueryForm.cs
@@ -0,0 +1,13 @@
+﻿namespace SofaFactory;
+
+public class FnDEnqueryForm
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string phone { get; set; }
+    public string address { get; set; }
+    public string town { get; set; }
+    public string DateOfEstablishment { get; set; }
+    public string Enquery { get; set; }
+    public string Additional { get; set; }
+}
